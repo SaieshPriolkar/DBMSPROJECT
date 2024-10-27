1638c152330a9e8cad0bcdc4286d84bbc990b158
SaieshPriolkar <saieshpriolkar@gmail.com>
2024-10-27 11:55:16 +0530

    Bhaissecond change

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Travel Hub</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <div class="header">
+    <div></div>
+    <div class="buttons">
+      <button class="button">Login</button>
+      <button class="button">Sign up</button>
+    </div>
+  </div>
+
+  <div class="main">
+    <div class="title">WELCOME TO TRAVEL HUB</div>
+  </div>
+
+
+</body>
+</html>
